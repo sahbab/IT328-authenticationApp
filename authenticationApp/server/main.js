ec82b255fb254162308e870c20aac9adf40927bd
Sahba Bahizad <sarahjune919@gmail.com>
2016-05-10 19:41:13 -0700

    added user validation

--- authenticationApp/server/main.js
+++ authenticationApp/server/main.js
@@ -1,5 +1,16 @@
 import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+  
+});
+
+
+Meteor.publish('userData', function(){
+	//if someone is logged in?
+	if( this.userId){
+		return Meteor.users.find({'_id' : this.userId});
+	}
+	else{
+		this.ready();//done
+	}
 });
